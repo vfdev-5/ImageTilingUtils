ed9569d1bfb52d2a27fb0f58f1312a573ec856c2
vfdev-5 <vfdev.5@gmail.com>
2018-06-01 21:32:57 +0200

    Update setup.py and travis.yml

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
+import os
 from codecs import open as codecs_open
+import re
 from setuptools import setup, find_packages
-from tiling import __version__
 
 
-# Get the long description from the relevant file
-with codecs_open('README.md', encoding='utf-8') as f:
-    long_description = f.read()
+def read(*names):
+    with codecs_open(os.path.join(os.path.dirname(__file__), *names),
+                     encoding="utf-8") as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+long_description = read("README.md")
+version = find_version('tiling', '__init__.py')
 
 
 setup(
     name="tiling",
-    version=__version__,
+    version=version,
     description=u"Minimalistic set of image reader agnostic tools to easily iterate over large images",
     long_description=long_description,
     author="vfdev-5",
     author_email="vfdev dot 5 at gmail dot com",
+    url="https://github.com/vfdev-5/ImageTilingUtils",
     packages=find_packages(exclude=['tests', 'examples']),
     install_requires=[
         'numpy',
